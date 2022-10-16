9caae25d1ca18e065a2357b9702817756904bfca
999Kit <kietnguyentuan911@gmail.com>
2022-10-16 14:24:40 +0700

    hmm1

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 
 int main() {
     srand(time(NULL));
+
     Console::SetScreenBufferSize(62, 25);
     Console::SetWindowSize(62, 25);
+
     cout << "kiet_pullRequest" << endl;
-    cout << "thaydoinuane" << endl;
     return 0;
 }
