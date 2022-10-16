c28769dbf28a87926c2626a25bfe90762ba88e56
999Kit <kietnguyentuan911@gmail.com>
2022-10-16 14:56:30 +0700

    kiet

--- main.cpp
+++ main.cpp
@@ -6,16 +6,5 @@ int main() {
 
     Console::SetScreenBufferSize(62, 25);
     Console::SetWindowSize(62, 25);
-    
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[100];
-        for (int i = 0; i < n; i++) cin >> a[i];
-        for (int i = 0; i < n; i++)  cout << a[i] << '\n';
-    }
-    cout << "kietCommitMaster";
     return 0;
 }
