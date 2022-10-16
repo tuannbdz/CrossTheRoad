9a7ada5f4301d5fc6e4aea664c89cba642188d67
Tuan Nguyen Bao <minecraftqn123@gmail.com>
2022-10-16 14:12:39 +0700

    a

--- main.cpp
+++ main.cpp
@@ -5,8 +5,6 @@ int main() {
     srand(time(NULL));
     Console::SetScreenBufferSize(80, 25);
     Console::SetWindowSize(80, 25);
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) cout << i << '\n';
+    cout << "TuanPush";
     return 0;
 }
