4e23a4eca82f5cb2f60415265032dd4ff13de940
Ngoc <dungtuyetngoc@gmail.com>
2022-11-26 19:10:53 +0700

    relocated number graphics

--- GAME.h
+++ GAME.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "lib.h"
 #include "Menu.h"
@@ -23,6 +23,70 @@ private:
 	vector< Car* > car;
 	vector< Shark* > shark;
 	vector< Truck* > tr;
+	const vector<vector<wstring>>numberGraphics = {
+		zero, one, two, three, four, five, six, seven, eight, nine
+	};
+
+	const vector<wstring> zero =
+	{
+		L"█▀█",
+		L"█▄█"
+	};
+
+	const vector<wstring> one =
+	{
+		L"▄█",
+		L" █"
+	};
+
+	const vector<wstring> two =
+	{
+		L"▀█",
+		L"█▄"
+
+	};
+
+	const vector<wstring> three =
+	{
+		L"▀██",
+		L"▄▄█"
+	};
+	const vector<wstring> four =
+	{
+		L"█ █",
+		L"▀▀█"
+
+	};
+	const vector<wstring> five =
+	{
+		L"█▀",
+		L"▄█"
+
+	};
+	const vector<wstring> six =
+	{
+		L"█▄▄",
+		L"█▄█"
+
+	};
+	const vector<wstring> seven =
+	{
+		L"▀▀█",
+		L"  █"
+
+	};
+	const vector<wstring> eight =
+	{
+		L"▄▀▀▄",
+		L"▄▀▀▄"
+		L"▀▄▄▀"
+	};
+
+	const vector<wstring> nine =
+	{
+		L"█▀█",
+		L"▀▀█"
+	};
 	//string data; //this is the filename that the user entered to save game process
 	
 public:
