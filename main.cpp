85688765ad9fc8d60a91b661a45f89abd93b48be
999Kit <kietnguyentuan911@gmail.com>
2022-12-20 22:11:11 +0700

    added singleton

--- main.cpp
+++ main.cpp
@@ -5,16 +5,17 @@
 #include <stdio.h>
 
 vector<wstring>g_board;
-
+Game* Game::instance = nullptr;
 int main() {
     _setmode(_fileno(stdout), _O_U16TEXT);
     srand(time(NULL));
     Console* console = new Console(1312, 700, L"Cross The Road", false, Graphics::GetColor(Color::brightwhite, Color::black)); 
-    //RunGame();
-    Game g;
-    g.Run();
-    delete console; 
-    //delete g;
+
+    Game* g = Game::GetInstance();
+    g->Run();
+
     //system("pause >nul"); 
+    delete console; 
+    Game::DeleteInstance();
     return 0;
 }
